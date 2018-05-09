59f87ad5652cae16e0855c511536857247ecfd55
Sam Getlan <samgetlan@gmail.com>
2018-05-09 09:56:31 -0700

    Hid newRelic Key

--- newrelic.js
+++ newrelic.js
@@ -1,3 +1,5 @@
+const newRelicKey = require('newRelicKey.js');
+
 'use strict'
 /**
  * New Relic agent configuration.
@@ -13,7 +15,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: '99acc99c5704956510b64859e1acb2ad6463f6ec',
+  license_key: newRelicKey,
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
